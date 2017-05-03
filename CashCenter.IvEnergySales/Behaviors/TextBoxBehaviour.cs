f308fc0d84c1686af70ccdfeb25c53d3ec635745
Vovanok <vovanok.net@gmail.com>
2017-05-03 10:27:26 +0300

    Increase usability. Select all on focus.

---
+++ CashCenter.IvEnergySales/Behaviors/TextBoxBehaviour.cs
@@ -0,0 +1,64 @@
+﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Input;
+
+namespace CashCenter.IvEnergySales.Behaviors
+{
+    public static class TextBoxBehavior
+    {
+        public static readonly DependencyProperty SelectAllTextOnFocusProperty =
+            DependencyProperty.RegisterAttached(
+                "SelectAllTextOnFocus",
+                typeof(bool),
+                typeof(TextBoxBehavior),
+                new UIPropertyMetadata(false, OnSelectAllTextOnFocusChanged));
+
+        public static bool GetSelectAllTextOnFocus(TextBox textBox)
+        {
+            return (bool)textBox.GetValue(SelectAllTextOnFocusProperty);
+        }
+
+        public static void SetSelectAllTextOnFocus(TextBox textBox, bool value)
+        {
+            textBox.SetValue(SelectAllTextOnFocusProperty, value);
+        }
+
+        private static void OnSelectAllTextOnFocusChanged(
+            DependencyObject dependencyObject, DependencyPropertyChangedEventArgs args)
+        {
+            var textBox = dependencyObject as TextBox;
+            if (textBox == null)
+                return;
+
+            if (!(args.NewValue is bool))
+                return;
+
+            if ((bool)args.NewValue)
+            {
+                textBox.GotFocus += SelectAll;
+                textBox.PreviewMouseDown += IgnoreMouseButton;
+            }
+            else
+            {
+                textBox.GotFocus -= SelectAll;
+                textBox.PreviewMouseDown -= IgnoreMouseButton;
+            }
+        }
+
+        private static void SelectAll(object sender, RoutedEventArgs e)
+        {
+            var textBox = e.OriginalSource as TextBox;
+            textBox?.SelectAll();
+        }
+
+        private static void IgnoreMouseButton(object sender, MouseButtonEventArgs args)
+        {
+            var textBox = sender as TextBox;
+            if (textBox == null || textBox.IsKeyboardFocusWithin)
+                return;
+
+            args.Handled = true;
+            textBox.Focus();
+        }
+    }
+}
