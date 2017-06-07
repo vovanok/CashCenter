7030d7d9bebe65a9c9d07aeec3f9db3685d344b2
Vovanok <vovanok.net@gmail.com>
2017-06-08 00:19:24 +0300

    View model for main window + versioning

---
+++ CashCenter.IvEnergySales/MainWindowViewModel.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Reflection;
+
+namespace CashCenter.IvEnergySales
+{
+    public class MainWindowViewModel
+    {
+        public Version Version { get; } = Assembly.GetExecutingAssembly().GetName().Version;
+    }
+}
