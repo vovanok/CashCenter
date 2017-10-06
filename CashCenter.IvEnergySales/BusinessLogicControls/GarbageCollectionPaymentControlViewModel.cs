f95d16edccf0da99be2d1131c9519bb9eaddd9af
Vovanok <vovanok.net@gmail.com>
2017-10-06 19:21:40 +0300

    Barcode parsing

--- CashCenter.IvEnergySales/BusinessLogicControls/GarbageCollectionPaymentControlViewModel.cs
+++ CashCenter.IvEnergySales/BusinessLogicControls/GarbageCollectionPaymentControlViewModel.cs
@@ -4,6 +4,7 @@ using CashCenter.Common.Exceptions;
 using CashCenter.IvEnergySales.BusinessLogic;
 using CashCenter.IvEnergySales.Common;
 using System;
+using System.Collections.Generic;
 using System.Windows;
 
 namespace CashCenter.IvEnergySales.BusinessLogicControls
@@ -41,15 +42,54 @@ namespace CashCenter.IvEnergySales.BusinessLogicControls
             ClearHandler(null);
         }
 
-        private void ApplyBarCode(string barCode)
+        private void ApplyBarCode(string barcode)
         {
-            // TODO: парсинг кода
-            CustomerNumber.Value = 0;
-            RegionCode.Value = 0;
-            FinacialPeriod.Value = 0;
-            OrganizationCode.Value = 0;
-            FilialCode.Value = 0;
-            Cost.Value = 0;
+            Log.Info(string.Format("Apply barcode: {0}", barcode));
+
+            if (string.IsNullOrEmpty(barcode) || barcode.Length != 30)
+            {
+                string errorMessage = "Штрих код не задан или имееет неверный формат";
+                Log.Error(string.Format("{0} ({1}; is null = {3})", errorMessage, barcode, barcode == null));
+                Message.Error(errorMessage);
+                return;
+            }
+
+            var errors = new List<string>();
+
+            string financialPeriodStr = barcode.Substring(0, 3);
+            if (!int.TryParse(barcode.Substring(0, 3), out int financialPeriod))
+                errors.Add($"Код финансового периода не корректен ({financialPeriodStr})");
+
+            string regionCodeStr = barcode.Substring(5, 2);
+            if (!int.TryParse(regionCodeStr, out int regionCode))
+                errors.Add($"Код региона не корректен ({regionCodeStr})");
+
+            string customerNumberStr = barcode.Substring(7, 10);
+            if (!int.TryParse(customerNumberStr, out int customerNumber))
+                errors.Add($"Лицевой счет не корректен ({customerNumberStr})");
+
+            string costStr = barcode.Substring(17, 9);
+            if (!int.TryParse(costStr, out int cost))
+                errors.Add($"Сумма не корректна ({costStr})");
+
+            string organizationCodeStr = barcode.Substring(26, 4);
+            if (!int.TryParse(organizationCodeStr, out int organizationCode))
+                errors.Add($"Код организации не корректен ({organizationCodeStr})");
+
+            if (errors.Count > 0)
+            {
+                string errorMessage = $"Ошибки в штрих-коде ({barcode}):\n{string.Join("\n", errors.ToArray())}";
+                Log.Error(errorMessage);
+                Message.Error(errorMessage);
+                return;
+            }
+
+            FinacialPeriod.Value = financialPeriod;
+            RegionCode.Value = regionCode;
+            CustomerNumber.Value = customerNumber;
+            Cost.Value = cost;
+            OrganizationCode.Value = organizationCode; ;
+            FilialCode.Value = 0; //?? TODO: from settings
         }
 
         private void PayHandler(object parameters)
