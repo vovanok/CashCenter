fd99da22234cac97f742c6bb1e2ee2f63b42c726
Vovanok <vovanok.net@gmail.com>
2017-11-27 10:29:38 +0300

    Empty lib project

---
+++ CashCenter.ZeusDal/ZeusContext.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CashCenter.ZeusDal
+{
+    public class ZeusContext
+    {
+        private string connectionString;
+
+        public ZeusContext(string connectionString)
+        {
+            this.connectionString = connectionString;
+        }
+
+
+    }
+}
