3a36e4236498628b6735fbd2705a8ab9b69b7ff0
Vovanok <vovanok.net@gmail.com>
2018-04-05 01:03:21 +0300

    Remove excess files

--- CashCenter.IvEnergySales/MainWindow.xaml.cs
+++ CashCenter.IvEnergySales/MainWindow.xaml.cs
@@ -3,10 +3,10 @@ using System.Windows;
 namespace CashCenter.IvEnergySales
 {
     public partial class MainWindow : Window
-	{
+    {
         public MainWindow()
-		{
-			InitializeComponent();
+        {
+            InitializeComponent();
         }
     }
 }
